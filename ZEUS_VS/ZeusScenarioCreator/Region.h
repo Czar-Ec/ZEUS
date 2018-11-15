45a801a44bf47f9705ef0965dea7090e3ba329e5
CzarEc <czar.echavez@yahoo.co.uk>
2018-11-15 22:22:01 +0000

    Setting up something

---
+++ ZEUS_VS/ZeusScenarioCreator/Region.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "../sharedobjects/Country.h"
+
+/**
+* Class Region
+* Each region can be part of a country, but not necessarily be inside the country
+* Alternate name territory
+*/
+class Region : public Country {
+
+};
